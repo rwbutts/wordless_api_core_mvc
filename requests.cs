e6c27786aa9f3f9a8a4a38c2ae419ddb6f921bea
Ronald W. (Bill) Butts, Jr <rwbutts@gmail.com>
2023-12-19 18:56:44 -0500

    refactor v 1.2.11

--- requests.cs
+++ requests.cs
@@ -1,17 +1,9 @@
 using System.ComponentModel.DataAnnotations;
 namespace WordlessAPI
 {
-     public record QueryMatchCountRequest
-     {
-          public string answer { get; set;}
-
-          public string[] guesses { get; set;}
-
-          public QueryMatchCountRequest( [MaxLength(5), MinLength(5)] string answer, string[] guesses )
-          {
-               this.guesses = guesses;
-               this.answer = answer;
-          }
-     }
+     public record QueryMatchCountRequest( 
+          [MaxLength(5), MinLength(5)] string answer, 
+          string[] guesses 
+     );
 }
  
